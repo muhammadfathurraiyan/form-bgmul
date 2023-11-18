944b3296d56abade2f80bf98504888f49b2410b3
muhammadfathurraiyan <raiyanfathur@gmail.com>
2023-11-18 23:46:36 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Form from "@/components/Form";
 import Home from "@/components/Home";
 import { useRouter } from "next/navigation";
 
-export default function page() {
+export default function Page() {
   const Router = useRouter();
   const Refresh = () => {
     Router.refresh();
